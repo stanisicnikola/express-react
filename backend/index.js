5a5561cbc033c3b4539020196ef7a891ed77ae6f
Nikola Stanisic <nikola@Nikolas-MacBook-Pro.local>
2025-06-07 15:29:59 +0200

    Added routes(get, post) for posts

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const port = 3000;
 const db = require("./models");
+
+//Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(port, () => {
